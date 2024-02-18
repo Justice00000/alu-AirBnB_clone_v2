d7aace9a7ad5b6ca7745243f856fd0bba0f56865
CeeJay <j.chukwuony@alustudent.com>
2024-02-18 07:08:46 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,4 +11,5 @@ echo "<h1>Chukwuonye Justice</h1>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
 sudo service nginx restart
