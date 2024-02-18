ac4241d444244944f79ed9a8b165aeeb3f624683
CeeJay <j.chukwuony@alustudent.com>
2024-02-18 06:51:28 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,36 +1,14 @@
 #!/usr/bin/env bash
-# A Bash script that sets up my web servers for the deployment of web_static
+# Write a Bash script that sets up your web servers for the deployment of web_static
 
-# Install Nginx if not already installed
-if ! command -v nginx &> /dev/null
-then
-    sudo apt-get update
-    sudo apt-get -y install nginx
-fi
+apt-get update
+apt-get -y install nginx
 
-# Create necessary folders if they don't exist
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 
-# Create fake HTML file for testing
-echo "<html>
-  <head>
-  </head>
-  <body>
-    <p>Hello, this is a test index.html file!</p>
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-
-# Create symbolic link
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
-
-# Set ownership recursively to ubuntu user and group
-sudo chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration
-sudo sed -i '/^\tlocation \/ {/a \
-\t\tlocation /hbnb_static/ {\
-\t\t\talias /data/web_static/current/;\
-\t\t}' /etc/nginx/sites-available/default
-
-# Restart Nginx
+echo "<h1>Chukwuonye Justice</h1>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 sudo service nginx restart
