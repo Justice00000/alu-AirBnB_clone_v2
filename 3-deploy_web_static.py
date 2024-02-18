7b6f24bdf4a7fe6e9b66e6005bd5cd757d1cdc50
Justice <j.chukwuony@alustudent.com>
2024-02-17 22:49:48 -0800

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.context_managers import cd
 from fabric.api import env, put, sudo, local
 from os.path import join, exists, splitext
 
-env.hosts = ["54.242.215.110", "34.229.154.33"]
+env.hosts = ["18.212.234.162", "54.226.115.250"]
 
 
 def do_pack():
