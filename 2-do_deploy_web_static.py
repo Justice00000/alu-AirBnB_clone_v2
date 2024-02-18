19211ecd8ef5847d5a90a0acd2f2bf9e89a17fbd
Justice <j.chukwuony@alustudent.com>
2024-02-17 22:20:38 -0800

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from os.path import join, exists, splitext
 
 
 env.user = "ubuntu"
-env.hosts = ["54.242.215.110", "34.229.154.33"]
+env.hosts = ["18.212.234.162", "54.226.115.250"]
 env.key_filename = '~/.ssh/id_rsa'
 
 
