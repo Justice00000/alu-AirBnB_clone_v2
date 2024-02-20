5c77c2808bd67e49d5c92810b06dfe32dcabc697
CeeJay <j.chukwuony@alustudent.com>
2024-02-20 09:02:47 +0100

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 from flask import Flask
 
 # Create a Flask application
@@ -7,9 +6,14 @@ app = Flask(__name__)
 # Define a route
 @app.route('/', strict_slashes=False)
 def hello():
+    """Display 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 # Run the Flask application
 if __name__ == '__main__':
+    # Start the server
+    print("Starting server...")
     app.run(host='0.0.0.0', port=5000)
 
+    # Stop the server
+    print("Stopping server...")
