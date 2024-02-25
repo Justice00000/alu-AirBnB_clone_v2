a5bec1937bec7bf17c1404fe88ae98430b861822
CeeJay <j.chukwuony@alustudent.com>
2024-02-25 16:03:23 +0100

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,35 +1,15 @@
 #!/usr/bin/python3
-"""
-0-hello_route.py
-~~~~~~~~~~~~~~~~
-This module implements a Flask web application with a single route that displays
-"Hello HBNB!" when accessed.
-
-Routes:
-    /: Displays "Hello HBNB!"
-
-Usage:
-    - Run this module directly to start the Flask web application:
-        ```
-        python3 -m web_flask.0-hello_route
-        ```
-"""
-
+"""Module that starts a Flask web application"""
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """
-    Route handler for the root URL.
-
-    Returns:
-        str: A greeting message.
-    """
-    return "Hello HBNB!"
+def index():
+    """Returns the string "Hello HBNB!" to the client"""
+    return 'Hello HBNB!'
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
